3c63fa0fa9f81cd63eab81adbe9298a2328faee4
Alamin Shaikh <hello@octib.com>
2023-03-06 15:45:48 +0600

    Max token number updated

--- config/llm.ts
+++ config/llm.ts
@@ -2,7 +2,7 @@ import { OpenAI } from "langchain/llms";
 
 // Configure OpenAI
 export const llm = new OpenAI({
-  maxTokens: 10,
+  maxTokens: 15,
   concurrency: 10,
   openAIApiKey: process.env.OPENAI_API_KEY,
 });
